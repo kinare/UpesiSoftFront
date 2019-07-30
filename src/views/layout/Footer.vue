cae3c325e9a9e01b5b76a48400e22d780a625174
Michael <michael.kamau@kinetics.co.ke>
2019-07-30 11:49:42 +0300

    Target blank for D4A link

--- src/views/layout/Footer.vue
+++ src/views/layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="footer">
         <div class="pull-right">
-            Made with <i class="fa fa-heart text-danger"></i>  <a href="https://digital4africa.com/" > by Digital 4 Africa</a>
+            Made with <i class="fa fa-heart text-danger"></i>  <a href="https://digital4africa.com/" target="_blank" > by Digital 4 Africa</a>
         </div>
         <div>
             <strong>Copyright</strong> Focus Glass & Alluminium © 2019
--- src/views/point of sale/Pos.vue
+++ src/views/point of sale/Pos.vue
@@ -38,7 +38,7 @@
         <div class="footer">
             <div class="footer">
                 <div class="pull-right">
-                    Made with <i class="fa fa-heart text-danger"></i>  <a href="https://digital4africa.com/" > by Digital 4 Africa</a>
+                    Made with <i class="fa fa-heart text-danger"></i>  <a href="https://digital4africa.com/" target="_blank" > by Digital 4 Africa</a>
                 </div>
                 <div>
                     <strong>Copyright</strong> Focus Glass & Alluminium © 2019
