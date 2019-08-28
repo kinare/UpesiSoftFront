c913140885629c371b2185e4f71be466da661df5
Michael <michael.kamau@kinetics.co.ke>
2019-08-28 21:56:21 +0300

    updates

--- src/views/accounting manager/Invoice.vue
+++ src/views/accounting manager/Invoice.vue
@@ -130,6 +130,110 @@
                                     </tbody>
                                 </table>
                             </div>
+                            <div class="invoice-box">
+                                <table cellpadding="0" cellspacing="0">
+                                    <tr class="top">
+                                        <td colspan="4">
+                                            <table>
+                                                <tr>
+                                                    <td class="title">
+                                                        <img src="https://www.focus.upesisoft.com/img/Focus%20Logo.png" style="width:100%; max-width:300px;">
+                                                    </td>
+
+                                                    <td>
+                                                        Invoice #: 123<br>
+                                                        Date: January 1, 2015<br>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+
+                                    <tr class="information">
+                                        <td colspan="4">
+                                            <table>
+                                                <tr>
+                                                    <td>
+                                                        Focus Glass & Alluminium.<br>
+                                                        Irene Njeri<br>
+                                                        injeri92@gmail.com
+                                                    </td>
+
+                                                    <td>
+                                                        <strong>To</strong><br>
+                                                        Nicholas Kimuli.<br>
+                                                        +254703826457<br>
+                                                        nicholaskimuli@gmail.com<br>
+                                                        Saachi Plaza, Along Argwings Kodhek Road
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr>
+
+                                    <tr class="heading">
+                                        <td>
+                                            Item
+                                        </td>
+
+                                        <td>
+                                            QTY
+                                        </td>
+
+                                        <td>
+                                            Unit
+                                        </td>
+
+                                        <td>
+                                            Price
+                                        </td>
+                                    </tr>
+
+                                    <tr class="item">
+                                        <td>
+                                            Flash Lock Siver
+                                        </td>
+
+                                        <td>
+                                            3
+                                        </td>
+
+                                        <td>
+                                            1M
+                                        </td>
+                                        <td>
+                                            KES 200.00
+                                        </td>
+                                    </tr>
+
+                                    <tr class="item last">
+                                        <td>
+                                            Flash Lock Siver
+                                        </td>
+
+                                        <td>
+                                            3
+                                        </td>
+
+                                        <td>
+                                            1M
+                                        </td>
+                                        <td>
+                                            KES 200.00
+                                        </td>
+                                    </tr>
+
+                                    <tr class="total">
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+
+                                        <td>
+                                            Total: KES 385.00
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
                         </div>
                     </div>
                     <div class="modal-footer">
@@ -232,5 +336,96 @@
 </script>
 
 <style scoped>
+    .invoice-box {
+        max-width: 800px;
+        min-height: 842px;
+        margin: auto;
+        padding: 30px;
+        border: 1px solid #eee;
+        box-shadow: 0 0 10px rgba(0, 0, 0, .15);
+        font-size: 16px;
+        line-height: 24px;
+        font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+        color: #555;
+    }
 
+    .invoice-box table {
+        width: 100%;
+        line-height: inherit;
+        text-align: left;
+    }
+
+    .invoice-box table td {
+        padding: 5px;
+        vertical-align: top;
+    }
+
+    .invoice-box table tr td:last-child {
+        text-align: right;
+    }
+
+    .invoice-box table tr.top table td {
+        padding-bottom: 20px;
+    }
+
+    .invoice-box table tr.top table td.title {
+        font-size: 45px;
+        line-height: 45px;
+        color: #333;
+    }
+
+    .invoice-box table tr.information table td {
+        padding-bottom: 40px;
+    }
+
+    .invoice-box table tr.heading td {
+        background: #eee;
+        border-bottom: 1px solid #ddd;
+        font-weight: bold;
+    }
+
+    .invoice-box table tr.details td {
+        padding-bottom: 20px;
+    }
+
+    .invoice-box table tr.item td{
+        border-bottom: 1px solid #eee;
+    }
+
+    .invoice-box table tr.item.last td {
+        border-bottom: none;
+    }
+
+    .invoice-box table tr.total td:last-child {
+        border-top: 2px solid #eee;
+        font-weight: bold;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .invoice-box table tr.top table td {
+            width: 100%;
+            display: block;
+            text-align: center;
+        }
+
+        .invoice-box table tr.information table td {
+            width: 100%;
+            display: block;
+            text-align: center;
+        }
+    }
+
+    /** RTL **/
+    .rtl {
+        direction: rtl;
+        font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+    }
+
+    .rtl table {
+        text-align: right;
+    }
+
+    .rtl table tr td:last-child {
+        text-align: left;
+    }
 </style>
